739950e135f41ab22f802d50e21298df9ed925fd
unixhero <c.friedel@outlook.com>
2019-01-10 08:43:50 +0100

    CSS-Spielerei

--- implementation/controller/login/LoginController.py
+++ implementation/controller/login/LoginController.py
@@ -1,5 +1,5 @@
 from implementation.controller import Controller
-import json
+import hashlib
 from implementation.controller.datenbank import DatenbankController
 from flask import render_template, request, session, redirect, url_for
 
@@ -17,8 +17,8 @@ class LoginController(Controller):
         if request.method == 'POST':
             db = DatenbankController()
 
-            self.superusers = db.get_login_information('SuperUser')
-            self.stations = db.get_login_information('Station')
+            self.superusers = db.get_login_information('superUser')
+            self.stations = db.get_login_information('station')
 
             return self.validate_login()
 
@@ -30,15 +30,24 @@ class LoginController(Controller):
 
     def validate_login(self):
         for account in self.superusers:
-            if request.form['login-username'] == account['Username'] and request.form['login-password'] == account['Passwort']:
+            if request.form['login-username'] == account['username'] and self.hash_password(request.form['login-password'], account['timestamp'], account['salt']) == account['password']:
                 session['username'] = request.form['login-username']
                 session['role'] = 'superUser'
                 return redirect(url_for('dashboard'))
 
         for account in self.stations:
-            if request.form['login-username'] == account['Stationname'] and int(request.form['login-password']) == account['PIN']:
+            if request.form['login-username'] == account['stationname'] and self.hash_password(request.form['login-password'], account['timestamp'], account['salt']) == account['pin']:
                 session['username'] = request.form['login-username']
                 session['role'] = 'station'
                 return redirect(url_for('dashboard'))
 
         return render_template('login/login.html', fehler=True)
+
+    def hash_password(self, password, timestamp, salt):
+        password_hash = hashlib.sha256(password.encode('utf-8'))
+        timestamp_hash = hashlib.sha256(str(timestamp).encode('utf-8'))
+        salt_hash = hashlib.sha256(str(salt).encode('utf-8'))
+
+        endhash = hashlib.sha256(password_hash.hexdigest().encode('utf-8') + timestamp_hash.hexdigest().encode(
+            'utf-8') + salt_hash.hexdigest().encode('utf-8'))
+        return endhash.hexdigest()
