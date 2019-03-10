183cd8f46310044cb4f784e0d0807a7e1741b99d
unixhero <c.friedel@outlook.com>
2019-03-10 13:40:26 +0100

    charts

---
+++ static/js/barchart2.js
@@ -0,0 +1,55 @@
+var ctx = document.getElementById("chartab").getContext('2d');
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [],
+        datasets: [
+        {
+            label: 'Schüler xa',
+            data: [12],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: 'Schüler ab',
+            data: [8],
+            backgroundColor: [
+                'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: 'Schüler xy',
+            data: [19],
+            backgroundColor: [
+
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }
+
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        responsive: true
+    }
+});
