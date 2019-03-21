61b48361ddb63ff733433f2723e57c783333e61d
maxma <max.may@live.de>
2019-03-21 19:16:43 +0100

    generates class number

--- src/packages/schoolclass/SchoolClass.py
+++ src/packages/schoolclass/SchoolClass.py
@@ -11,8 +11,15 @@ class SchoolClass(AbstractSchoolClass):
 
     def add_class(self):
         db = DatabaseController()
+
+        # DO-WHILE ???
+        while True:
+            number = randint(100, 999)
+            if db.get_class_information({'number': number}) is None:
+                break
+
         #login = Login()
         #auth = {'timestamp': time.time(), 'salt': randint(10, 99), 'pin': ''}
 
         #auth['pin'] = login.hash_password(request.form['pin'], auth['timestamp'], auth['salt'])
-        return db.insert_class(request.form)
+        return db.insert_class(request.form, number)
